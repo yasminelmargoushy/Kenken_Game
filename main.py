8634ac73b81a17518417c10bd3d080db20e8823e
mariamelshaer <73904088+mariamelshaer@users.noreply.github.com>
2022-06-03 23:36:37 +0200

    create main.py
    Co-Authored-By: Nada-Ibrahim-Amer <73901405+Nada-Ibrahim-Amer@users.noreply.github.com>

--- main.py
+++ main.py
@@ -94,3 +94,163 @@ def ken_generator(size):
         cages[-1] = (tuple(cages[-1]), op, int(target))
 
     return size, cages
+    
+    
+def parsing_game(rows):
+    if isinstance(rows, str):
+        rows = rows.splitrows(True)
+
+    try:
+        content = rows[0][:-1]
+        size = int(content)
+    except:
+        exit(11)
+
+    cages = []
+    for line in rows[1:]:
+        content = line[:-1]
+        if content:
+            try:
+                cl = eval(content)
+                cages.append(cl)
+            except:
+                exit(12)
+
+    return size, cages
+
+def Valid(size, cages):
+    outOfBounds = lambda xy: xy[0] < 1 or xy[0] > size or xy[1] < 1 or xy[1] > size
+
+    mentioned = set()
+    for i in range(len(cages)):
+        members, op, target = cages[i]
+
+        cages[i] = (tuple(set(members)), op, target)
+
+        members, op, target = cages[i]
+
+        if op not in "+-*/.":
+            exit(1)
+
+        problematic = list(filter(outOfBounds, members))
+        if problematic:
+            exit(2)
+
+        problematic = mentioned.intersection(set(members))
+        if problematic:
+            exit(3)
+
+        mentioned.update(set(members))
+
+    indexes = range(1, size + 1)
+
+    problematic = set([(x, y) for y in indexes for x in indexes]).difference(mentioned)
+
+    if problematic:
+        exit(4)
+
+
+def conflicting(X, x, Y, y):
+    for i in range(len(X)):
+        for j in range(len(Y)):
+            mA = X[i]
+            mB = Y[j]
+            ma = x[i]
+            mb = y[j]
+            if (mA[0] == mB[0]) != (mA[1] == mB[1]) and ma == mb:
+                return True
+
+    return False
+
+def validated(values, operation, target):
+    for p in permutations(values):
+        if reduce(operation, p) == target:
+            return True
+
+    return False
+
+
+def Domain(size, cages):
+
+    domains = {}
+    for cl in cages:
+        members, op, target = cl
+
+        domains[members] = list(product(range(1, size + 1), repeat=len(members)))
+
+        qualifies = lambda values: not conflicting(members, values, members, values) and validated(values, operation(op), target)
+
+        domains[members] = list(filter(qualifies, domains[members]))
+
+    return domains
+
+def Neighbour(cages):
+    neighbors = {}
+    for members, _, _ in cages:
+        neighbors[members] = []
+
+    for A, _, _ in cages:
+        for B, _, _ in cages:
+            if A != B and B not in neighbors[A]:
+                if conflicting(A, [-1] * len(A), B, [-1] * len(B)):
+                    neighbors[A].append(B)
+                    neighbors[B].append(A)
+
+    return neighbors
+
+class Ken_Ken_Game(csp.CSP):
+
+    def __init__(self, size, cages):
+        Valid(size, cages)
+        
+        variables = [members for members, _, _ in cages]
+        
+        domains = Domain(size, cages)
+
+        neighbors = Neighbour(cages)
+
+        csp.CSP.__init__(self, variables, domains, neighbors, self.my_constarints)
+
+        self.size = size
+        self.checks = 0
+
+    def my_constarints(self, A, a, B, b):
+        self.checks += 1
+
+        return A == B or not conflicting(A, a, B, b)
+
+
+def solve_100_games(kenken, algorithm):
+        kenken.checks = kenken.nassigns = 0
+        dt = time()
+        assignment = algorithm(kenken)
+        dt = time() - dt
+        return assignment, (kenken.checks, kenken.nassigns, dt)
+
+def solve_100_board(output_file):
+    bt         = lambda ken: csp.backtracking_search(ken)
+    fc         = lambda ken: csp.backtracking_search(ken, inference=csp.forward_checking)
+    mac        = lambda ken: csp.backtracking_search(ken, inference=csp.mac)
+
+    algorithms = {
+        "BT": bt,
+        "FC": fc,
+        "MAC": mac
+    }
+
+    with open(output_file, "w+") as file:
+
+        output_file = writer(file)
+
+        output_file.writerow(["Algorithm", "Size", "Time"])
+        for i in range (6):
+            for name, algorithm in algorithms.items():
+                for size in range(3, 9):
+                    checks, assignments, dt = (0, 0, 0)
+                    current_time1 = timer()
+                    size, cages = ken_generator(size)
+
+                    assignment, data = solve_100_games(Ken_Ken_Game(size, cages), algorithm)
+                    current_time2 = timer()
+                    my_time = current_time2 - current_time1
+                    output_file.writerow([name, size, my_time])
